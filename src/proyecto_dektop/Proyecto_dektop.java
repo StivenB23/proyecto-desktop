dddae55b3a3f2ba09a7d482f49b49ebcdaeec533
stive <example@gmail.com>
2022-08-12 11:39:30 -0500

    Structure

---
+++ src/proyecto_dektop/Proyecto_dektop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_dektop;
+
+/**
+ *
+ * @author Aprendiz
+ */
+public class Proyecto_dektop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
